7c306658a8447e332f006331a6a26d5ed9b0149c
bhavik senjaliya <bhavik.softcolon@gmail.com>
2026-01-28 12:49:46 +0530

    add admin route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import express from "express"
 import authRouter from "./auth.route.js"
 import taskRouter from "./task.route.js"
+import adminRouter from "./admin.route.js"
 
 const router = express.Router()
 
 
 router.use("/auth" , authRouter)
 router.use("/task" , taskRouter)
+router.use("/admin" , adminRouter)
 
 export default router
 
